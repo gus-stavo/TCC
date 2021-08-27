e1d448910b3666a0fa20eed2bc6d50b2d828cfde
kaireleaokk <kaireleao@hotmail.com>
2021-08-27 17:34:47 -0300

    Update commonStyles.js

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -3,7 +3,39 @@ export default {
         azul: 'rgba(111, 222, 552, 1)',
         preto: '#222',
         branco: '#eee'
-    }
+    },
+    icones: {
+        width: 40,
+        height: 40,
+        font: 40,
+    },
+    botaoAzul: {
+        fontSize: 20,
+        fontFamily: 'Exo2-Medium',
+        borderRadius: 10,
+        borderColor: '#222',
+        width: 120,
+        borderWidth: 1,
+        elevation: 5,
+    },
+    titulo: {
+        fontSize: 70,
+        marginBottom: 10,
+        fontFamily: "Exo2-SemiBoldItalic",
+        color: '#222',
+    },
+    ContainerPrincipal: {
+        width: '87%',
+        height: '92%',
+        borderWidth: 0.5,
+        borderRadius: 10,
+        elevation: 50
+    },
+    containerFora: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%'
+      },
 }
     
 
